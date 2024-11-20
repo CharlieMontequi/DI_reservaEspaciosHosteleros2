3056728f21257d526cb433b68a3baf98742f2e9c
carlos <carlos.montequi.rodriguez@iesjulianmarias.es>
2024-11-20 14:42:54 +0100

    prierm comit

---
+++ app/src/main/java/com/example/di_reservaespacioshosteleros/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.di_reservaespacioshosteleros
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        /** TODO
+         * implementar recogida de datos
+         * crear un layout nuevo en el que se vaya al formulario de registro
+         * implementar toast de los campos obligatorios, que van a ser todos
+         * al enviar mostrar los datos en una ventana emergente y pedir confirmacion
+         * al borrar pedir confirmacion de que quiere salir sin guardar
+         * añadir funionalidad a los botones
+         * cancelar vuelve a la actividad anterior y borra todos los campos
+         */
+    }
+}
